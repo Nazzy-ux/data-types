a7c979e318425b32fe4610a4eb586f40a21136da
Nazzy-ux <nazneendeklerk001@gmail.com>
2020-03-10 10:19:41 +0200

    declaring data types

---
+++ data types.py
@@ -0,0 +1,27 @@
+x= "Good Morning"
+print(type(x))#declaring a string variable
+print(x[1])#extracting certain data out of a str data type
+print(x[2:5]) #extracting 2 of 5 data types out of a str
+
+intnumber1=5 #displaying an interger
+intnumber2=5.8 #displaying a float
+intnumber3=10j #displaying a complex number
+print(type(intnumber1)) #testing data type int
+print(type(intnumber2))#testing data type float
+print(type(intnumber3)) #testing data type complex
+
+mylist=[23,82,42,"Andrew"]
+print(type(mylist))
+
+_list=[23, 56 ,77, 89, 99]
+print(type(_list))
+
+
+_list.insert(3,12)
+print(_list)
+
+cars={"ashton martin","vw","ford","bmw","Nissan"}
+print(type(cars))
+
+cars1=("audi","hundai","honda","chevrolet")
+print(type(cars1))
